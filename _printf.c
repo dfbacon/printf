ae831168ccfa6bdf06b8decc610517d53ce1328b
Daniel Bacon <daniel.bacon@holbertonschool.com>
2016-11-22 21:40:43 +0000

    fixing bad tests

--- _printf.c
+++ _printf.c
@@ -12,7 +12,8 @@ int string_app(char *base, char *str, int location)
 {
 	if (str == NULL)
 	{
-		return (location);
+		_strcat(base, "(null)");
+		return (location + 1);
 	}
 	else
 	{
@@ -51,8 +52,8 @@ int _printf(const char *format, ...)
 	int i, n, temp, size;
 	char buffer[1024];
 
-	if (format == NULL)
-		return (0);
+/*	if (format == NULL) */
+/*		return (0); */
 	size = 0;
 	va_start(args, format);
 	i = n = temp = 0;
