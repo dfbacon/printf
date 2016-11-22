9f1d69ac14cc9f3b25afdb409516d9673ef08a4b
Daniel Bacon <daniel.bacon@holbertonschool.com>
2016-11-22 21:37:33 +0000

    testing NULL

--- _printf.c
+++ _printf.c
@@ -51,8 +51,8 @@ int _printf(const char *format, ...)
 	int i, n, temp, size;
 	char buffer[1024];
 
-/*	if (format == NULL) */
-/*		return (0); */
+	if (format == NULL)
+		return (0);
 	size = 0;
 	va_start(args, format);
 	i = n = temp = 0;
