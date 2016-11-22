12a27b60581e535b45a12c0144eeb5e9bf63e7a2
Daniel Bacon <daniel.bacon@holbertonschool.com>
2016-11-22 07:59:01 +0000

    more formatting issues

--- _printf.c
+++ _printf.c
@@ -1,6 +1,46 @@
 #include "holberton.h"
 
 /**
+ * string_app - append two strings
+ * @base: initial string
+ * @str: string to add
+ * @location: position in format (i.e format[location])
+ *
+ * Return: pointer to appended base
+ */
+int string_app(char *base, char *str, int location)
+{
+	if (str == NULL)
+	{
+		_strcat(base, "(null)");
+		return (location + 1);
+	}
+	else
+	{
+		_strcat(base, str);
+		return (location + 1);
+	}
+}
+
+/**
+ * num_app - append string with converted number
+ * @base: initial string
+ * @num: number to be converted and added
+ * @location: location in format (i.e. format[location])
+ *
+ * Return: pointer to appended base
+ */
+int num_app(char *base, int num, int location)
+{
+	char temp[100];
+
+	/* convert number to string */
+	itoa(num, temp);
+	string_app(base, temp, 0);
+	return (location + 1);
+}
+
+/**
  * _printf - replicate the printf function
  * @format: character string
  *
